6aae0156f18db8605df808df10361e9a5d26df40
zero2black <miuizeref@gmail.com>
2024-08-19 06:19:38 +0700

    Android Sync Mon Aug 19 06:19:37 WIB 2024

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ PACKAGES+=" zram-swap adb parted losetup resize2fs luci luci-ssl block-mount luc
 PACKAGES+=" internet-detector luci-app-internet-detector vnstat2 vnstati2 luci-app-netmonitor"
 
 # Speedtest
-PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 luci-app-netspeedtest"
+PACKAGES+=" luci-app-speedtest-web"
 
 
 
